7ceab7eeb570dd32072c60360bacac62642456cd
James Pederson <james@jpederson.com>
2015-04-03 13:35:34 -0500

    Remove header wrapper.

--- header.php
+++ header.php
@@ -23,16 +23,12 @@
 
 <header>
 
-	<div class="wrap">
-
-		<div class="logo">
-			<a href="/" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<img src="<?php bloginfo( "template_url" ) ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>">
-			</a>
-		</div>
-	
+	<div class="logo">
+		<a href="/" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<img src="<?php bloginfo( "template_url" ) ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>">
+		</a>
 	</div>
-
+	
 	<nav role="navigation">
 		<!--<div class="wrap">-->
 			<button class="menu-toggle"><i class="fa fa-bars"></i></button>
