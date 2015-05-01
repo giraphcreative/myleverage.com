4820e5c320feba6b3a06f6160f798db81ecfceec
James Pederson <james@jpederson.com>
2015-05-01 00:40:49 -0500

    Add widget areas from LSCU to Leverage so they're on both sites.

--- library/widgets.php
+++ library/widgets.php
@@ -11,6 +11,22 @@ if ( function_exists('register_sidebar') ) {
         'before_title' => '<div class="widget-title"><h4>',
         'after_title' => '</h4></div>',
     ));
+ 	register_sidebar(array(
+		'name'=> '404 Sidebar',
+		'id' => 'sidebar-404',
+        'before_widget' => '<div class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<div class="widget-title"><h4>',
+        'after_title' => '</h4></div>',
+    ));
+ 	register_sidebar(array(
+		'name'=> 'Homepage Events',
+		'id' => 'home-events',
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ));
 }
 
 
