0982bbb984a7879f3edb088dc655fd766151e2f5
James Pederson <james@jpederson.com>
2015-05-06 11:18:47 -0500

    Remove extra closing div tag in footer.

--- footer.php
+++ footer.php
@@ -10,40 +10,38 @@
  */
 ?>	
 	
-		</div>
+	</div>
 
-	</section>
-	
-	<footer class="footer">
-		<div class="wrap">
-			<div class="column first">
-				<h3>Connect With Us</h3>
-				<p class="address">22 Inverness Cntr Pky, #200<br>
-					Birmingham, Alabama 35242</p>
-				<p class="address">3692 Coolidge Court<br>
-					Tallahassee, Florida 32311</p>
-				<p><a href="tel:8662310545">866.231.0545</a></p>
-				<div class="social">
-					<a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-twitter.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-facebook.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-google.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-linkedin.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-youtube.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-pinterest.png"></a>
-				</div>
-			</div>
-			<div class="column">
-				<h3>Links</h3>
-				<nav role="navigation">
-					<?php wp_nav_menu( array( 
-						'theme_location' => 'footer', 
-						'menu_class' => 'nav-menu' ) 
-					); ?>
-				</nav>
-			</div>
-			<div class="column">
-				<h3>Sign Up For News</h3>
-				<?php do_shortcode( '[snippet slug="subscribe" /]' ); ?>
+</section>
+
+<footer class="footer">
+	<div class="wrap">
+		<div class="column first">
+			<h3>Connect With Us</h3>
+			<p class="address">22 Inverness Cntr Pky, #200<br>
+				Birmingham, Alabama 35242</p>
+			<p class="address">3692 Coolidge Court<br>
+				Tallahassee, Florida 32311</p>
+			<p><a href="tel:8662310545">866.231.0545</a></p>
+			<div class="social">
+				<a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-twitter.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-facebook.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-google.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-linkedin.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-youtube.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-pinterest.png"></a>
 			</div>
 		</div>
-	</footer><!-- #colophon -->
-
-</div><!-- #page -->
+		<div class="column">
+			<h3>Links</h3>
+			<nav role="navigation">
+				<?php wp_nav_menu( array( 
+					'theme_location' => 'footer', 
+					'menu_class' => 'nav-menu' ) 
+				); ?>
+			</nav>
+		</div>
+		<div class="column">
+			<h3>Sign Up For News</h3>
+			<?php do_shortcode( '[snippet slug="subscribe" /]' ); ?>
+		</div>
+	</div>
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 </body>
