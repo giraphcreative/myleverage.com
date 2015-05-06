e9a9b2a865fa3756eb1c3becad2ef83e8dda0913
James Pederson <james@jpederson.com>
2015-05-06 09:33:34 -0500

    Add homepage news widget area.

--- library/widgets.php
+++ library/widgets.php
@@ -28,6 +28,14 @@ if ( function_exists('register_sidebar') ) {
         'after_title' => '</h4></div>',
     ));
  	register_sidebar(array(
+		'name'=> 'Homepage News',
+		'id' => 'home-news',
+        'before_widget' => '<div class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<div class="widget-title"><h4>',
+        'after_title' => '</h4></div>',
+    ));
+ 	register_sidebar(array(
 		'name'=> 'Homepage Twitter Feed',
 		'id' => 'home-twitter',
         'before_widget' => '<div class="widget %2$s">',
