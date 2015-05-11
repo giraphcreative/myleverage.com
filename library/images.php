05df87ae12ef03cb8da5887455551a10078a929f
James Pederson <james@jpederson.com>
2015-05-11 11:35:01 -0500

    Don't automatically link images that are inserted into the page.

--- library/images.php
+++ library/images.php
@@ -123,4 +123,16 @@ function p_is_image( $img_path ) {
 }
 
 
+
+function wpb_imagelink_setup() {
+    $image_set = get_option( 'image_default_link_type' );
+    
+    if ($image_set !== 'none') {
+        update_option('image_default_link_type', 'none');
+    }
+}
+add_action('admin_init', 'wpb_imagelink_setup', 10);
+
+
+
 ?>
