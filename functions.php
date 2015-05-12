bb7f7e65eb8d448ef0f12982395a90ec0eea8148
James Pederson <james@jpederson.com>
2015-05-12 13:09:43 -0500

    Enable oEmbed in text widgets.

--- functions.php
+++ functions.php
@@ -29,4 +29,9 @@ include( "library/showcase.php" );
 include( "library/accordion.php" );
 
 
+// enable oembed and shortcodes in text widgets
+add_filter( 'widget_text', array( $wp_embed, 'run_shortcode' ), 8 );
+add_filter( 'widget_text', array( $wp_embed, 'autoembed'), 8 );
+
+
 ?>
