d174efd052f8b062e5828cb6b842e26be9c81c54
James Pederson <james@jpederson.com>
2015-05-12 13:33:18 -0500

    Add an anchor shortcode to help add anchor names in.

--- functions.php
+++ functions.php
@@ -29,6 +29,13 @@ include( "library/showcase.php" );
 include( "library/accordion.php" );
 
 
+// [anchor] shortcode
+function p_anchor( $atts, $content = null, $code = "" ) {
+    return '<a name="'.$content.'"></a>';
+}
+add_shortcode('anchor' , 'p_anchor' );
+
+
 // enable oembed and shortcodes in text widgets
 add_filter( 'widget_text', array( $wp_embed, 'run_shortcode' ), 8 );
 add_filter( 'widget_text', array( $wp_embed, 'autoembed'), 8 );
