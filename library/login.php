c8acd5b2ce6d056d47273daf6f0dec87bbeb8e56
James Pederson <james@jpederson.com>
2015-05-12 09:13:43 -0500

    Add new login hook to check a different password format - to accommodate EZ publish auth style.

--- library/login.php
+++ library/login.php
@@ -25,4 +25,27 @@ function account_toolbox() {
 }
 
 
+
+// additional login functionality
+add_filter( 'authenticate', 'lscu_signon', 30, 3 );
+function lscu_signon( $user, $username, $password ) {
+	if ( isset( $_POST['log'] ) ) {
+		$creds = array(
+			'user_login' => $_POST['log'],
+			'user_password' => $_POST['log'] . "\n" . $_POST['pwd'],
+			'remember' => true
+		);
+		$user_check = get_user_by( 'login', $_POST['log'] );
+		
+		if ( $user_check->user_pass == md5( $_POST['log'] . "\n" . $_POST['pwd'] ) ) {
+			return $user_check;
+		}
+	}
+
+	// return the user
+    return $user;
+}
+
+
+
 ?>
