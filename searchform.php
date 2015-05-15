7b25911b6640cc39ce87c6bc27d03e26f4affdf8
James Pederson <james@jpederson.com>
2015-05-14 21:06:30 -0500

    Don't filter search to only areas.

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
 <form role="search" method="get" id="searchform" class="searchform" action="/" _lpchecked="1">
-	<input type="hidden" name="post_type" value="area" />
 	<input type="text" value="" name="s" id="s" placeholder="Search">
 	<input type="submit" id="searchsubmit" value="Search">
 </form>
