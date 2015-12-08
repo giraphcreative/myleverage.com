423b03ebc73acf1c79a49f867fea37ad7b75bb04
James Pederson <james@jpederson.com>
2015-12-08 16:13:17 -0600

    Set it up so there's a 'no events' note on the events list if there aren't any upcoming events.

--- page-front.php
+++ page-front.php
@@ -25,17 +25,19 @@ get_header();
 				$wpdb->show_errors();
 
 				$events = $wpdb->get_results( "SELECT * FROM `lscu_posts` `posts` LEFT JOIN `lscu_term_relationships` `termrel` ON posts.ID = termrel.object_id LEFT JOIN `lscu_term_taxonomy` `termtax` ON termtax.term_taxonomy_id = termrel.term_taxonomy_id LEFT JOIN `lscu_postmeta` `pm` ON posts.ID = pm.post_id WHERE posts.post_type='event' AND posts.post_status='publish' AND termtax.term_id = 144 AND pm.meta_key = '_p_event_start' AND pm.meta_value>=" . mktime( 0, 0, 0 ) . " ORDER BY `meta_value` ASC;" );
-
-				foreach ( $events as $event ) {
-					$event_link = $wpdb->get_results( "SELECT * FROM `lscu_postmeta` WHERE post_id = " . $event->ID . " AND meta_key = '_p_event_website';" );
-					?>
-					<article>
-						<h4><a href="<?php print ( !empty( $event_link[0]->meta_value ) ? $event_link[0]->meta_value : $event->guid ) ?>" target="_blank"><?php print $event->post_title ?></a></h4>
-						<?php print date( 'n/j/Y', $event->meta_value ); ?>
-					</article>
-					<?php
+				if ( !empty( $events ) ) {
+					foreach ( $events as $event ) {
+						$event_link = $wpdb->get_results( "SELECT * FROM `lscu_postmeta` WHERE post_id = " . $event->ID . " AND meta_key = '_p_event_website';" );
+						?>
+						<article>
+							<h4><a href="<?php print ( !empty( $event_link[0]->meta_value ) ? $event_link[0]->meta_value : $event->guid ) ?>" target="_blank"><?php print $event->post_title ?></a></h4>
+							<?php print date( 'n/j/Y', $event->meta_value ); ?>
+						</article>
+						<?php
+					}
+				} else {
+					?><article><p>There are currently no upcoming <strong><em>LEVERAGE</em></strong> events.</p></article><?php
 				}
-
 				?>
 				</div>
 				<a class="home-third-button link-events" href="http://www.lscu.coop/events/?event_category=144">All Events</a>
