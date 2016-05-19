0308d7b97ac59019ac57dda6baf387afa0f99eb9
James Pederson <james@jpederson.com>
2016-05-19 11:22:02 -0500

    Move iframe resize function so '$' exists when it runs.

--- js/src/main.js
+++ js/src/main.js
@@ -1,24 +1,6 @@
 
 
 
-// resize linqto iframe
-var resize_iframe = function () {
-	if ( matchMedia('only screen and ( min-width: 1152px )').matches ) {
-		$('iframe.auto-height').height( '1950px' );
-	} else if ( matchMedia('only screen and ( min-width: 892px )').matches ) {
-		$('iframe.auto-height').height( '2430px' );
-	} else if ( matchMedia('only screen and ( min-width: 823px )').matches ) {
-		$('iframe.auto-height').height( '3630px' );
-	} else if ( matchMedia('only screen and ( min-width: 768px )').matches ) {
-		$('iframe.auto-height').height( '2930px' );
-	} else if ( matchMedia('only screen and ( min-width: 655px )').matches ) {
-		$('iframe.auto-height').height( '2430px' );
-	} else if ( matchMedia('only screen and ( min-width: 600px )').matches ) {
-		$('iframe.auto-height').height( '3630px' );
-	} else if ( matchMedia('only screen and ( max-width: 599px )').matches ) {
-		$('iframe.auto-height').height( '2930px' );
-	}
-}
 
 
 // onload responsive footer and menu stuff
@@ -114,6 +96,26 @@ jQuery(document).ready(function($){
 	
 	// creep on links
 	$(".wrap a").creep();
+
+
+	// resize linqto iframe
+	var resize_iframe = function(){
+		if ( matchMedia('only screen and ( min-width: 1152px )').matches ) {
+			$('iframe.auto-height').height( '1950px' );
+		} else if ( matchMedia('only screen and ( min-width: 892px )').matches ) {
+			$('iframe.auto-height').height( '2430px' );
+		} else if ( matchMedia('only screen and ( min-width: 823px )').matches ) {
+			$('iframe.auto-height').height( '3630px' );
+		} else if ( matchMedia('only screen and ( min-width: 768px )').matches ) {
+			$('iframe.auto-height').height( '2930px' );
+		} else if ( matchMedia('only screen and ( min-width: 655px )').matches ) {
+			$('iframe.auto-height').height( '2430px' );
+		} else if ( matchMedia('only screen and ( min-width: 600px )').matches ) {
+			$('iframe.auto-height').height( '3630px' );
+		} else if ( matchMedia('only screen and ( max-width: 599px )').matches ) {
+			$('iframe.auto-height').height( '2930px' );
+		}
+	}
 
 
 	// resize linqto iframe
