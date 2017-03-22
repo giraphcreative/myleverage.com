54e691ea95dd944ecc96be7a3735ad4979ea8894
James Pederson <james@jpederson.com>
2017-03-22 07:10:43 -0500

    Update events dash icon.

--- library/post-type/event.php
+++ library/post-type/event.php
@@ -44,7 +44,7 @@ function lscu_events() {
 			'show_ui' => true,
 			'query_var' => true,
 			'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */ 
-			'menu_icon' => get_stylesheet_directory_uri() . '/img/icon-admin-event.png', /* the icon for the custom post type menu */
+			'menu_icon' => 'dashicons-calendar-alt', /* the icon for the custom post type menu */
 			'rewrite'	=> array( 
 				'slug' => 'event', 
 				'with_front' => false 
