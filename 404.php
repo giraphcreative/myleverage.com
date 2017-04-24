70959578449ff26f9d8f68ea26f6f7ec512bcf7c
James Pederson <james@jpederson.com>
2017-04-24 18:34:02 -0500

    Add better 404 template.

--- 404.php
+++ 404.php
@@ -3,26 +3,23 @@
  * The template for displaying 404 pages (Not Found)
  */
 
-get_header(); ?>
+get_header(); 
+
+?>
+
+	<div id="content" class="wrap group content-wide <?php print $color ?>" role="main">
 
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+		<div class="page-content">
+			<h1 class="page-title"><?php _e( 'Page Not Found', 'twentyfourteen' ); ?></h1>
+			<p>It looks like nothing was found at this location. Look for the page in the top menu - several of our pages have been reorganized. Or maybe try a search?</p>
+			<p><?php get_search_form(); ?></p>
+		</div><!-- .page-content -->
 
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'twentyfourteen' ); ?></h1>
-			</header>
-
-			<div class="page-content">
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfourteen' ); ?></p>
-
-				<?php get_search_form(); ?>
-			</div><!-- .page-content -->
-
-		</div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar( 'content' );
-get_sidebar();
+
 get_footer();
+
+?>
