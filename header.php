3a04dc56439fe0d3305cd637f6b27f2e6fcf9129
James Pederson <james@jpederson.com>
2017-05-11 21:30:12 -0500

    Fix conditional html tags.

--- header.php
+++ header.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
-<!--[if IE 7]><html class="ie ie7" <?php language_attributes(); ?>><![endif]-->
-<!--[if IE 8]><html class="ie ie8" <?php language_attributes(); ?>><![endif]-->
-<!--[if IE & !(IE 7) & !(IE 8) ]><html class="ie" <?php language_attributes(); ?>><![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
+<!--[if IE ]><html class="ie" <?php language_attributes(); ?>><![endif]-->
+<!--[if !IE ]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width,initial-scale=1" />
