6ac53f8ae946fa8bfa57210826b7de01495ffc3e
James Pederson <james@jpederson.com>
2017-05-11 21:42:18 -0500

    Add IE styles.

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!--[if IE ]><html class="ie" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE ]><html class="ie-legacy" <?php language_attributes(); ?>><![endif]-->
 <!--[if !IE ]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
