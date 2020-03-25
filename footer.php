088164a33f85aa5e73cb18fc141c773359c43ee7
James Pederson <james@jpederson.com>
2020-03-25 13:09:09 -0500

    Update footer containers to fit the third address better.

--- footer.php
+++ footer.php
@@ -26,20 +26,20 @@ if ( empty( $color ) ) $color = 'forest';
 </div>
 
 <footer class="footer">
-		<div class="column first">
+		<div class="two-third first">
 			<h3>Connect With Us</h3>
-			<div class="sub-column first">
+			<div class="column third text-right first">
 				3692 Coolidge Court<br />
 				Tallahassee, Florida 32311<br>
 				6705 Sugarloaf Pky, #200<br>
 				Duluth, Georgia
 			</div>
-			<div class="sub-column text-center">
+			<div class="third text-center">
 				22 Inverness Cntr Pky, #200<br />
 				Birmingham, Alabama<br />
 				866.231.0545</p>
 			</div>
-			<div class="sub-column">
+			<div class="third text-left">
 				6705 Sugarloaf Pky, #200<br>
 				Duluth, Georgia
 			</div>
@@ -47,12 +47,12 @@ if ( empty( $color ) ) $color = 'forest';
 				<a href="https://twitter.com/LeagueofSECUs" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-twitter.png"></a><a href="https://www.facebook.com/LeagueofSoutheasternCreditUnions?ref=hl" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-facebook.png"></a><a href="https://www.linkedin.com/company/1007350?trk=tyah&trkInfo=clickedVertical%3Acompany%2Cidx%3A1-1-1%2CtarId%3A1428961608364%2Ctas%3Amy+leverage" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-linkedin.png"></a><a href="https://www.youtube.com/user/LeagueofSECUs" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-youtube.png"></a>
 			</div>
 		</div>
-		<div class="column">
+		<div class="third">
 			<h3>Links</h3>
-			<div class="sub-column first">
+			<div class="half first">
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-one' ) ); ?>	
 			</div>
-			<div class="sub-column">
+			<div class="half">
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-two' ) ); ?>
 			</div>
 		</div>
